a2b5d129b427b53a250d0f6a5e6139eafe15f915
lani-irving <laniirving@googlemail.com>
2023-03-15 22:46:50 +0000

    basic form prototype, started styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [answer, setAnswer] = useState("");
+
+  function handleSubmit() {
+    // style alert
+    // submit answer - save to database or a file
+    if (answer !== "") {
+      alert("Your next location is....");
+      setAnswer("");
+    } else if (answer === "") {
+      alert("Please submit an answer!");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="question-container">
+      <form id="quiz-form">
+        <h1>Submit your answer to reveal your next location!</h1>
+        <h2>
+          If your scavenger hunt partner could have one superpower, what would
+          they choose?
+        </h2>
+        <textarea
+          id="answer-input"
+          // rows="3"
+          onChange={(event) => setAnswer(event.target.value)}
+          value={answer}
+        ></textarea>
+        <button type="submit" id="submit-button" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
